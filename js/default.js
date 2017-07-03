fbebc3c05bc5efaa99a102f55ac2d04dbfc6611c
g1627007 <g1627007@st.t-kougei.ac.jp>
2017-07-03 17:23:46 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,6 +6,7 @@ var separate_time = function(time){
   return [sec, min, hours, days];
 }
 
+var update = function(){
 var now = new Date();
 var target = new Date(2018,1,1,0,0,0,0);
 var diff = target.getTime() - now.getTime();
@@ -15,4 +16,11 @@ document.getElementById('countdown').textContent =
   counter[3] + '日' +
   counter[2] + '時' +
   counter[1] + '分' +
-  counter[0] + '秒' +;
+  counter[0] + '秒' ;
+ refresh();
+}
+
+var refresh= function(){
+  setTimeout(update, 1000);
+}
+upfate();
