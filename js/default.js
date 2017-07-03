e8787f696f4fb3c01e311805ae4e098c87507f89
g1627007 <g1627007@st.t-kougei.ac.jp>
2017-07-03 18:26:57 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -25,13 +25,3 @@ var refresh= function(){
 }
 upfate();
                  
-document.getElementById('form').select.onchange = function(){
-  location.href = document.getElementById('form').select.value;
-}
-
-var thmubs = document.querySelectorAll('.thumb');
-for(idx in thmuns){
-    thmbs[idx].onclick = function(){
-      document.getElementById("bigimg").src = 'img/' + this.dateset.image+ '.jpg';
-    }
-}
